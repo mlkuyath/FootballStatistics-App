6a51158de311e848ae2a44607046ce269719370a
Matthew Kuyath <matthewkuyath@Matthews-MacBook-Air.local>
2020-05-28 12:47:38 -0500

    8

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 var __dirname = path.resolve(path.dirname(''));
 
 
-var cors = require('cors');
-
-app.use(cors());
-
 app.set('port', PORT);
 app.set('env', NODE_ENV);
 app.use(logger('tiny'));
