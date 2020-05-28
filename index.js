8b32a472366bae7d7aa07ae02efd2021809bdd30
Matthew Kuyath <matthewkuyath@Matthews-MacBook-Air.local>
2020-05-28 12:25:33 -0500

    4th commit

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-var __dirname = path.resolve(path.dirname(''));
+// Import the library:
+var cors = require('cors');
+// Then use it before your routes are set up:
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 app.set('port', PORT);
 app.set('env', NODE_ENV);
