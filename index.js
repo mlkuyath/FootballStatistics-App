68d416e68e912429f5bda1a308e86f6eb5a245a8
Matthew Kuyath <matthewkuyath@Matthews-MacBook-Air.local>
2020-05-28 12:42:37 -0500

    6

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-
 var __dirname = path.resolve(path.dirname(''));
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 app.set('port', PORT);
 app.set('env', NODE_ENV);
